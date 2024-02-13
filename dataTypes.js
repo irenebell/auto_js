fb30fa16eebb0a0e0b59c05c84831ced2d9cbde2
irenebell <irinaselen1@gmail.com>
2024-02-13 21:16:35 +0200

    Added referencesTypes.js

--- dataTypes.js
+++ dataTypes.js
@@ -5,3 +5,20 @@ let boolean = true;
 let nullValue = null;
 let undefinedvalue;
 let sym = Symbol();
+
+const first ="Hello";
+const second = 'World';
+result = first + " " + second;
+console.log(result)
+
+/**Complex; objects and arrfays**/
+
+const obj = {
+  name: 'John',
+  lastName: 'Doe'
+};
+
+const arr = [1, 2, '1', '2'];
+
+
+
