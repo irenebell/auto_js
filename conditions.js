7ae2bc4d11be0967dd1e29c88014e877167a197b
irenebell <irinaselen1@gmail.com>
2024-02-15 20:32:20 +0200

    Added operations.js, conditions.js

---
+++ conditions.js
@@ -0,0 +1,6 @@
+const x = 7;
+if (x === 5) {
+  console.log('this is true');
+} else {
+    console.log("False");
+};
---
+++ operations.js
@@ -0,0 +1,22 @@
+
+let pow = 2 ** 3;
+let mod = 8 % 3;
+
+console.log('pow =', pow);
+console.log('mod =', mod);
+
+let i = 1;
+let k = ++i;
+let n = i++;
+console.log('k =', k);
+console.log('i =', i);
+console.log('n =', n);
+
+let j = 1;
+j--;
+--j;
+console.log('j =', j);
+
+let str = typeof "John";
+console.log(str);
+
